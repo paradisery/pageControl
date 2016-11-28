1e83af0897a4f1a2a9936b42a566ce55258a46bc
ruany <ruany@ruanydeiMac.local>
2016-11-28 16:38:56 +0800

    Initial Commit

---
+++ pageControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pageControl
+//
+//  Created by ruany on 2016/11/28.
+//  Copyright © 2016年 ruany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ pageControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  pageControl
+//
+//  Created by ruany on 2016/11/28.
+//  Copyright © 2016年 ruany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
